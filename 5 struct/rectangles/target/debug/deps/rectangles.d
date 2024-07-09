1fc9a004a7eb50eb7a483ba23ce1d3e63690cca1
key4127 <2827092384@qq.com>
2024-07-09 09:33:27 +0800

    chapter 5

---
+++ 5 struct/rectangles/target/debug/deps/rectangles.d
@@ -0,0 +1,5 @@
+E:\The Rust Programming Language\5 struct\rectangles\target\debug\deps\rectangles.exe: src/main.rs
+
+E:\The Rust Programming Language\5 struct\rectangles\target\debug\deps\rectangles.d: src/main.rs
+
+src/main.rs:
---
+++ 5 struct/rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+E:\The\ Rust\ Programming\ Language\5\ struct\rectangles\target\debug\rectangles.exe: E:\The\ Rust\ Programming\ Language\5\ struct\rectangles\src\main.rs
